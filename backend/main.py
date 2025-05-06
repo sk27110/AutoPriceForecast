0f70e36a7fcb25059284e74d49154c38007ef33f
sk27110 <sashakoch27@gmail.com>
2025-05-06 18:21:16 +0300

    Добавление возможности выбора типа модели
    К линейной регрессии добавились Ridge и Lasso, теперь можно обучать их

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ from sklearn.model_selection import train_test_split
 from fastapi import FastAPI, HTTPException, BackgroundTasks, Query, UploadFile, File
 import io
 from sklearn.metrics import r2_score
-
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 
 
 
@@ -41,10 +41,6 @@ numerical_cols = ['year', 'mileage', 'engine_capacity', 'engine_power', 'travel_
 categorical_cols = ['title', 'transmission', 'body_type', 'drive_type', 'color', 'fuel_type']
 
 
-df = X_train[:1000]
-df.to_csv('dataset.csv', index=False)
-
-
 class TitleExtractor(BaseEstimator, TransformerMixin):
     def __init__(self, column):
         self.column = column
@@ -60,7 +56,6 @@ class TitleExtractor(BaseEstimator, TransformerMixin):
         return X
 
 
-
 app = FastAPI(title="Linear Model API")
 
 
@@ -69,11 +64,40 @@ ACTIVE_MODEL_ID: Optional[str] = None
 
 
 class TrainParams(BaseModel):
-    id: str
-    fit_intercept: bool = Field(default=True)
-    normalize: bool = Field(default=False) 
-    n_jobs: Optional[int] = Field(default=None)
+    None
 
+
+class LinearRegressionTrainParams(TrainParams):
+    fit_intercept: bool = Field(default=True, description="Indicates whether to fit the intercept term.")
+    n_jobs: Optional[int] = Field(default=None, description="Number of processors to use for computation. If None, all available processors are used.")
+    copy_X: bool = Field(default=True, description="If True, the input data X will be copied. If False, they may be modified in place.")
+
+
+class RidgeTrainParams(TrainParams):
+    alpha: float = Field(default=1.0, description="Regularization strength; must be a positive float.")
+    fit_intercept: bool = Field(default=True, description="Whether to fit the intercept.")
+    max_iter: Optional[int] = Field(default=None, description="Maximum number of iterations for optimization.")
+    solver: str = Field(default='auto', description="Solver to use for optimization.")
+    tol: float = Field(default=0.0001, description="Tolerance for the optimization; smaller values lead to more precise convergence.")
+
+
+class LassoTrainParams(TrainParams):
+    alpha: float = Field(default=1.0, description="Regularization strength; must be a positive float.")
+    fit_intercept: bool = Field(default=True, description="Whether to fit the intercept.")
+    max_iter: Optional[int] = Field(default=1000, description="Maximum number of iterations for optimization.")
+    selection: str = Field(default='cyclic', description="If 'cyclic', the updates will be done in a cyclic fashion.")
+    tol: float = Field(default=0.0001, description="Tolerance for the optimization; smaller values lead to more precise convergence.")
+
+
+
+class ModelType(BaseModel):
+    model_type: str = Field(default="LinearRegression", description="Type of linear model")
+
+
+
+
+class ModelID(BaseModel):
+    id: str = Field("new_model")
 
 class PredictInput(BaseModel):
     data: List[Dict[str, float]]
@@ -82,24 +106,20 @@ class PredictInput(BaseModel):
 class ModelInfo(BaseModel):
     id: str
     fit_intercept: bool
-    normalize: bool
     is_active: bool
 
 
 
-def build_pipeline(params: TrainParams) -> Pipeline:
+def build_pipeline(params: TrainParams, model_type: ModelType) -> Pipeline:
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
         ('scaler', StandardScaler())
     ])
-
-
     categorical_transformer = Pipeline(steps=[
         ('title_extractor', TitleExtractor(column='title')),
         ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
         ('onehot', OneHotEncoder(handle_unknown='ignore'))
     ])
-
     column_trans = ColumnTransformer(
         [
             ('cat', categorical_transformer, categorical_cols),
@@ -107,45 +127,99 @@ def build_pipeline(params: TrainParams) -> Pipeline:
         ],
         remainder='drop'
     )
+    model_cls_map = {
+        "LinearRegression": LinearRegression,
+        "Ridge": Ridge,
+        "Lasso": Lasso
+    }
 
-    model = LinearRegression(
-        fit_intercept=params.fit_intercept,
-        n_jobs=params.n_jobs
-    )
+    model_cls = model_cls_map[model_type.model_type]
+    # Преобразование Pydantic модели в словарь
+    model_params = params.model_dump()
+    model = model_cls(**model_params)
+    
+    pipeline = Pipeline(steps=[
+        ('preprocessor', column_trans),
+        ('classifier', model)
+    ])
+    return pipeline  # Возвращаем созданный пайплайн
 
-    pipeline = Pipeline(steps=[('preprocessor', column_trans),
-                      ('classifier', model)])
-    return pipeline
 
-
-@app.post("/fit", response_model=ModelInfo)
-def train_model(params: TrainParams, background_tasks: BackgroundTasks):
-    def train_job(params: TrainParams, model_id: str):
+@app.post("/fit_linear", response_model=ModelInfo)
+def train_linear_model(params: LinearRegressionTrainParams, id: ModelID, background_tasks: BackgroundTasks):
+    model_type = ModelType(model_type="LinearRegression")
+    def train_job(params: LinearRegressionTrainParams, model_id: str, model_type):
         start = time.time()
-        try:
-            pipeline = build_pipeline(params)
-            print("true")
-            pipeline.fit(X_train, y_train)
-            y_train_pred = pipeline.predict(X_train)
-            print('r2_score на трейне', r2_score(y_train, y_train_pred))
-            y_test_pred = pipeline.predict(X_test)
-            print('r2_score на трейне', r2_score(y_test, y_test_pred))
-            if time.time() - start <= 10:
-                MODELS[model_id] = pipeline
-                joblib.dump(pipeline, MODEL_STORAGE / f"{model_id}.pkl")
-        except Exception as e:
-            print("Warning")
+        # try:
+        pipeline = build_pipeline(params, model_type=model_type)
+        pipeline.fit(X_train, y_train)
+        if time.time() - start <= 10:
+            MODELS[model_id] = pipeline
+            joblib.dump(pipeline, MODEL_STORAGE / f"{model_id}.pkl")
+        # except Exception as e:
+        #     print("Warning")
 
 
-    model_id = params.id
-    background_tasks.add_task(train_job, params, model_id)
+    model_id = id.id
+    background_tasks.add_task(train_job, params, model_id, model_type)
 
     return ModelInfo(
         id=model_id,
         fit_intercept=params.fit_intercept,
-        normalize=params.normalize,
         is_active=False
     )
+
+
+
+@app.post("/fit_ridge", response_model=ModelInfo)
+def train_ridge_model(params: RidgeTrainParams, id: ModelID, background_tasks: BackgroundTasks):
+    model_type = ModelType(model_type="Ridge")
+    def train_job(params: RidgeTrainParams, model_id: str, model_type):
+        start = time.time()
+        # try:
+        pipeline = build_pipeline(params, model_type=model_type)
+        pipeline.fit(X_train, y_train)
+        if time.time() - start <= 10:
+            MODELS[model_id] = pipeline
+            joblib.dump(pipeline, MODEL_STORAGE / f"{model_id}.pkl")
+        # except Exception as e:
+        #     print("Warning")
+
+
+    model_id = id.id
+    background_tasks.add_task(train_job, params, model_id, model_type)
+
+    return ModelInfo(
+        id=model_id,
+        fit_intercept=params.fit_intercept,
+        is_active=False
+    )
+
+
+
+@app.post("/fit_lasso", response_model=ModelInfo)
+def train_lasso_model(params: LassoTrainParams, id: ModelID, background_tasks: BackgroundTasks):
+    model_type = ModelType(model_type="Lasso")
+    def train_job(params: LassoTrainParams, model_id: str, model_type):
+        start = time.time()
+        # try:
+        pipeline = build_pipeline(params, model_type=model_type)
+        pipeline.fit(X_train, y_train)
+        MODELS[model_id] = pipeline
+        joblib.dump(pipeline, MODEL_STORAGE / f"{model_id}.pkl")
+        # except Exception as e:
+        #     print("Warning")
+
+
+    model_id = id.id
+    background_tasks.add_task(train_job, params, model_id, model_type)
+
+    return ModelInfo(
+        id=model_id,
+        fit_intercept=params.fit_intercept,
+        is_active=False
+    )
+
 
 
 @app.post("/predict")
@@ -157,7 +231,6 @@ async def predict(file: UploadFile = File(...)):
     try:
         contents = await file.read()
         df = pd.read_csv(io.BytesIO(contents))
-        print(True)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error reading CSV: {str(e)}")
 
@@ -182,7 +255,6 @@ def list_models():
         ModelInfo(
             id=model_id,
             fit_intercept=model.named_steps["classifier"].fit_intercept,
-            normalize=False,
             is_active=(model_id == ACTIVE_MODEL_ID)
         ) for model_id, model in MODELS.items()
     ]
