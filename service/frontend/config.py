301f3a2c7381e22365d22a29478f3d57462ff8fd
Egor Albov <albov.egor@gmail.com>
2025-06-15 13:59:40 +0300

    refactored steamlit app code

---
+++ service/frontend/config.py
@@ -0,0 +1,9 @@
+import os
+
+BASE_API_URL = os.getenv("BASE_API_URL")
+
+STREAMLIT_CONFIG = {
+    "page_title": "Car Price Prediction",
+    "layout": "wide",
+    "initial_sidebar_state": "expanded"
+}
